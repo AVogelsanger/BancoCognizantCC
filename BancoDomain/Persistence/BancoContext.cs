b4cb6d9b2d8e716707e41e423181f13dfc14f33e
748418 <748418@LTBR67606>
2019-06-11 20:42:30 -0300

    Projeto Banco Cog

---
+++ BancoDomain/Persistence/BancoContext.cs
@@ -0,0 +1,21 @@
+﻿using BancoInfra.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BancoInfra.Persistence
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext() : base("BancoContext") { }
+
+        public DbSet<ContaCorrente> ContasCorrentes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ BancoInfra/Models/ContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BancoInfra.Models
+{
+    public class ContaCorrente
+    {
+        public int ID { get; set; }
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public double Saldo { get; set; }
+    }
+}
